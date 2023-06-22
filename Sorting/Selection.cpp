dc53a395791f89b79c6c584d5b9118240990c3f3
MOHDSAIFGIT <mohdsaifb173@gmail.com>
2023-06-22 22:01:07 +0530

    first commit

---
+++ Sorting/Selection.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n],UI;
+
+    for(int i=0;i<n;i++){
+        cin>>UI;
+        arr[i]=UI;
+        for(int j;j<n;i++){
+            
+        }
+    }
+    // for(int i=0;i<n;i++){
+    //     cout<<arr[i]<<" "<<endl;
+    // }
+
+
+    return 0;
+}
